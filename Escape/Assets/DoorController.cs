b1f148f260f6413ea49cc41cecc873e1b0f5bc54
marcompp <marcompp@al.insper.edu.br>
2023-03-06 10:44:29 -0300

    fx

--- Escape/Assets/DoorController.cs
+++ Escape/Assets/DoorController.cs
@@ -11,11 +11,15 @@ public class DoorController : MonoBehaviour
     private int[] inputed = {0,0,0,0};
     private bool open = false;
     static int input;
+    public AudioSource soundSource;
+    public AudioClip success;
+    public AudioClip buzzer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        soundSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,15 +44,13 @@ public class DoorController : MonoBehaviour
             }
             if (inputed[3] != 0) {
                 if (inputed.SequenceEqual(password)) {
-                    soundSource.clip=success; 
-                    soundSource.Play();
+                    soundSource.PlayOneShot(success);
 
                     open = true;
                     rb.velocity = new Vector3(0,2,0);
                 }
                 else {
-                    soundSource.clip=buzzer; 
-                    soundSource.Play();
+                    soundSource.PlayOneShot(buzzer);
 
                     inputed = new int[] {0,0,0,0};
                 }
